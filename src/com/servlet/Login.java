61efdc825ae6132f0c5f4ee928b37f924624a2e2
chefvivica <vivicacorn@gmail.com>
2020-12-21 15:13:01 -0500

    first commit

---
+++ src/com/servlet/Login.java
@@ -0,0 +1,54 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String email = request.getParameter("email");
+		String pass= request.getParameter("password");
+		
+		 HttpSession session=request.getSession();  
+	     session.setAttribute("email",  email);
+	     session.setAttribute("password",  pass);
+
+	                 
+		if(email.equals("viv@gmail.com") && pass.equals("123")) {
+			response.sendRedirect("DashboardServlet");  
+		}else {
+			response.getWriter().write("sorry, please try again");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
